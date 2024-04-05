c8df1a50b3a2fa0f90b6c6d3771a638f9f0f169b
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-05 19:18:09 -0400

    Update OG Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-nomy.vercel.app",
+  site: "https://devplace.pages.dev/",
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
