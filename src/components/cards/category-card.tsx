c0182e8526a85a9afc209041b9501a13c7450a99
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-04-08 09:01:48 -0400

    style: :lipstick: Improve Category Card Design

--- src/components/cards/category-card.tsx
+++ src/components/cards/category-card.tsx
@@ -17,11 +17,11 @@ export const CategoryCard: React.FC<Props> = ({
     <>
       <a
         href={href}
-        className="bg-background-200 hover:ring-2 hover:ring-primary transition-all even:md:max-w-64 min-w-44 md:max-w-72 flex-1 p-4 rounded"
+        className="bg-background-200 hover:ring-2 hover:ring-primary transition-all even:md:max-w-64 min-w-44 md:max-w-72 flex-1 p-8 md:p-6 rounded-xl"
       >
         {children}
         <p className="font-heading text-2xl mt-4">{title}</p>
-        <p>{description}</p>
+        <p className="text-sm text-neutral-400">{description}</p>
       </a>
     </>
   );
